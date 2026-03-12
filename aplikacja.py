e783e5eb0a719c46017c2080a98304296fecddc7
Taurus-PL <wojciech.gurgul@icloud.com>
2026-03-12 16:03:25 +0100

    Update aplikacja.py

--- aplikacja.py
+++ aplikacja.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import pandas as pd
-import re
-import unicodedata
+impor streamlit as st
+impor pandas as pd
+impor re
+impor unicodedata
  
  # --- KONFIGURACJA STRONY ---
  st.set_page_config(page_title="Zapis Przebiegu Terapii CBT", layout="wide")
