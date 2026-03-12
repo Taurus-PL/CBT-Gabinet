d1ade62559aa5b09bff3cc0f71fda43f1cdb225f
Taurus-PL <wojciech.gurgul@icloud.com>
2026-03-12 16:11:11 +0100

    Remove unused import statement
    Removed unused import for unicodedata.

--- aplikacja.py
+++ aplikacja.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import re
-+import unicodedata
  
  # --- KONFIGURACJA STRONY ---
  st.set_page_config(page_title="Zapis Przebiegu Terapii CBT", layout="wide")
